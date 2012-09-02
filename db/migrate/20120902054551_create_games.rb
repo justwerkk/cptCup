0b342429c424320f8de604d8d38486721a2ebf53
Tony <tonylc@gmail.com>
2012-09-01 22:58:09 -0700

    add players schema

--- db/migrate/20120902054551_create_games.rb
+++ db/migrate/20120902054551_create_games.rb
@@ -1,10 +1,10 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-      t.string :playerOneWinner
-      t.string :playerTwoWinner
-      t.string :playerOneLoser
-      t.string :playerTwoLoser
+      t.integer :winner_one_id
+      t.integer :winner_two_id
+      t.integer :loser_one_id
+      t.integer :loser_two_id
       t.integer :cupsLeft
 
       t.timestamps
