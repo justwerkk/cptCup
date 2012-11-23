064464646a426bad1a64d17b23ec3b91b2888d34
Tony <tonylc@gmail.com>
2012-11-22 21:27:56 -0800

    hook up w/ real data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,5 +10,20 @@ class HomeController < ApplicationController
   end
 
   def comparisons
+    game_hash = {}
+    Game.all.each do |g|
+      game_hash[g.winner_one_id] = PlayerComparison.new unless game_hash[g.winner_one_id]
+      game_hash[g.winner_two_id] = PlayerComparison.new unless game_hash[g.winner_two_id]
+      game_hash[g.loser_one_id] = PlayerComparison.new unless game_hash[g.loser_one_id]
+      game_hash[g.loser_two_id] = PlayerComparison.new unless game_hash[g.loser_two_id]
+
+      game_hash[g.winner_one_id].add_wins_against(g.loser_one_id, g.loser_two_id)
+      game_hash[g.winner_two_id].add_wins_against(g.loser_one_id, g.loser_two_id)
+      game_hash[g.loser_one_id].add_loses_against(g.winner_one_id, g.winner_two_id)
+      game_hash[g.loser_two_id].add_loses_against(g.winner_one_id, g.winner_two_id)
+    end
+    @game_hash = {}
+    game_hash.to_a.each {|arr| @game_hash[arr.first] = arr.last.against_hash.to_a}
+    @players_hash = Player.all.inject({}) {|hash, p| hash[p.id] = p.name; hash}
   end
 end
---
+++ app/models/player_comparison.rb
@@ -0,0 +1,23 @@
+class PlayerComparison
+  attr_reader :against_hash
+
+  def initialize
+    @against_hash = {}
+  end
+
+  def add_loses_against(winner_one_id, winner_two_id)
+    @against_hash[winner_one_id] = 0 unless @against_hash[winner_one_id]
+    @against_hash[winner_two_id] = 0 unless @against_hash[winner_two_id]
+
+    @against_hash[winner_one_id] -= 1
+    @against_hash[winner_two_id] -= 1
+  end
+
+  def add_wins_against(loser_one_id, loser_two_id)
+    @against_hash[loser_one_id] = 0 unless @against_hash[loser_one_id]
+    @against_hash[loser_two_id] = 0 unless @against_hash[loser_two_id]
+
+    @against_hash[loser_one_id] += 1
+    @against_hash[loser_two_id] += 1
+  end
+end
