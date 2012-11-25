0070f5c590f37389f3dfc9c82490c5f3a9a629cf
Tony <tonylc@gmail.com>
2012-11-25 02:24:59 -0800

    add W-L record

--- app/models/player_comparison.rb
+++ app/models/player_comparison.rb
@@ -6,18 +6,18 @@ class PlayerComparison
   end
 
   def add_loses_against(winner_one_id, winner_two_id)
-    @against_hash[winner_one_id] = 0 unless @against_hash[winner_one_id]
-    @against_hash[winner_two_id] = 0 unless @against_hash[winner_two_id]
+    @against_hash[winner_one_id] = [0,0] unless @against_hash[winner_one_id]
+    @against_hash[winner_two_id] = [0,0] unless @against_hash[winner_two_id]
 
-    @against_hash[winner_one_id] -= 1
-    @against_hash[winner_two_id] -= 1
+    @against_hash[winner_one_id][1] += 1
+    @against_hash[winner_two_id][1] += 1
   end
 
   def add_wins_against(loser_one_id, loser_two_id)
-    @against_hash[loser_one_id] = 0 unless @against_hash[loser_one_id]
-    @against_hash[loser_two_id] = 0 unless @against_hash[loser_two_id]
+    @against_hash[loser_one_id] = [0,0] unless @against_hash[loser_one_id]
+    @against_hash[loser_two_id] = [0,0] unless @against_hash[loser_two_id]
 
-    @against_hash[loser_one_id] += 1
-    @against_hash[loser_two_id] += 1
+    @against_hash[loser_one_id][0] += 1
+    @against_hash[loser_two_id][0] += 1
   end
 end
