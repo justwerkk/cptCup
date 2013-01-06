d7eb0c8049122e26d0dfefddfa609e1aff79fd8a
Lijen Tan <lijentan@gmail.com>
2013-01-05 16:46:18 -0800

    reverting hacked migrations

--- db/migrate/20130105061341_create_first_league.rb
+++ db/migrate/20130105061341_create_first_league.rb
@@ -1,10 +1,8 @@
 class CreateFirstLeague < ActiveRecord::Migration
   def up
-=begin
     first_league = League.create(name: "Canuto Cup Season 1",
                                  start_date: Date.new(2012, 10, 6))
-=end
-    first_league = League.first
+
     Game.all.each {|game| game.update_attribute(:league_id, first_league.id)}
   end
 
