1ba5ef2049c1cd069eb12da04e8642d5699951d5
Lijen Tan <lijentan@gmail.com>
2013-01-05 16:17:25 -0800

    fix

--- db/migrate/20130105061341_create_first_league.rb
+++ db/migrate/20130105061341_create_first_league.rb
@@ -4,6 +4,7 @@ class CreateFirstLeague < ActiveRecord::Migration
     first_league = League.create(name: "Canuto Cup Season 1",
                                  start_date: Date.new(2012, 10, 6))
 =end
+    first_league = League.first
     Game.all.each {|game| game.update_attribute(:league_id, first_league.id)}
   end
 
