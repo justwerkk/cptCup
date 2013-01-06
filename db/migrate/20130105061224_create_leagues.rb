3e470f17b805887f6398c6d4134efd55ca365f69
Lijen Tan <lijentan@gmail.com>
2013-01-05 16:14:29 -0800

    fixing fucked data

--- db/migrate/20130105061224_create_leagues.rb
+++ db/migrate/20130105061224_create_leagues.rb
@@ -1,11 +1,12 @@
 class CreateLeagues < ActiveRecord::Migration
   def up
+=begin
     create_table :leagues do |t|
       t.string :name, required: true
       t.date :start_date, nil: true
       t.date :end_date, nil: true
     end
-
+=end
     add_column :games, :league_id, :integer, required: true
   end
 
--- db/migrate/20130105061341_create_first_league.rb
+++ db/migrate/20130105061341_create_first_league.rb
@@ -1,8 +1,9 @@
 class CreateFirstLeague < ActiveRecord::Migration
   def up
+=begin
     first_league = League.create(name: "Canuto Cup Season 1",
                                  start_date: Date.new(2012, 10, 6))
-
+=end
     Game.all.each {|game| game.update_attribute(:league_id, first_league.id)}
   end
 
