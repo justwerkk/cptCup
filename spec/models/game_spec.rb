01bd82de264d466926b41368b9591e86cf646d88
Lijen Tan <lijentan@gmail.com>
2013-07-17 00:07:03 -0700

    first successful passing test with factory girl and rspec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Game do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can't include the same player twice" do
+    p1 = create(:player)
+    p2 = create(:player)
+    p3 = create(:player)
+
+    g = build(:game, winner_one: p1, winner_two: p1, loser_one: p2, loser_two: p2)
+    g.should_not be_valid
+    g.should have(1).error_on(:base)
+
+    g = build(:game, winner_one: p1, winner_two: p2, loser_one: p2, loser_two: p3)
+    g.should_not be_valid
+    g.should have(1).error_on(:base)
+
+    g = build(:game, winner_one: p1, winner_two: p2, loser_one: p3, loser_two: p3)
+    g.should_not be_valid
+    g.should have(1).error_on(:base)
+
+    g = build(:game, winner_one: p1, winner_two: p2, loser_one: p3, loser_two: p1)
+    g.should_not be_valid
+    g.should have(1).error_on(:base)
+  end
+
+  pending "it calculates rankings of players"
+  pending "it calculates the total winner score"
+  pending "it calculates the total loser score"
+  pending "it calculates rankings"
 end
