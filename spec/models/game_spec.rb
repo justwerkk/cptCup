99ba35d704adee2ca5f6411efdce6d06147df888
Lijen Tan <lijentan@gmail.com>
2013-07-17 01:48:08 -0700

    moving player generation to before

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,53 +1,53 @@
 require 'spec_helper'
 
 describe Game do
+  before(:each) do
+    @p1 = create(:player)
+    @p2 = create(:player)
+    @p3 = create(:player)
+    @p4 = create(:player)
+  end
+
   it "has a valid factory" do
     build(:game).should be_valid
   end
 
   it "should be invalid if a player appears twice" do
-    p1 = create(:player)
-    p2 = create(:player)
-    p3 = create(:player)
-
-    g = build(:game, winner_one: p1, winner_two: p1, loser_one: p2, loser_two: p2)
+    g = build(:game, winner_one: @p1, winner_two: @p1, loser_one: @p2, loser_two: @p2)
     g.should_not be_valid
     g.should have(1).error_on(:base)
 
-    g = build(:game, winner_one: p1, winner_two: p2, loser_one: p2, loser_two: p3)
+    g = build(:game, winner_one: @p1, winner_two: @p2, loser_one: @p2, loser_two: @p3)
     g.should_not be_valid
     g.should have(1).error_on(:base)
 
-    g = build(:game, winner_one: p1, winner_two: p2, loser_one: p3, loser_two: p3)
+    g = build(:game, winner_one: @p1, winner_two: @p2, loser_one: @p3, loser_two: @p3)
     g.should_not be_valid
     g.should have(1).error_on(:base)
 
-    g = build(:game, winner_one: p1, winner_two: p2, loser_one: p3, loser_two: p1)
+    g = build(:game, winner_one: @p1, winner_two: @p2, loser_one: @p3, loser_two: @p1)
     g.should_not be_valid
     g.should have(1).error_on(:base)
   end
 
   context "when calculating scores for a list of games" do
     it "calculates players scores" do
-      p1 = create(:player)
-      p2 = create(:player)
-      p3 = create(:player)
-      p4 = create(:player)
-
       games = []
-      games << build(:game, winner_one: p1, winner_two: p2, loser_one: p3, loser_two: p4)
-      games << build(:game, winner_one: p1, winner_two: p3, loser_one: p2, loser_two: p4)
-      games << build(:game, winner_one: p3, winner_two: p4, loser_one: p2, loser_two: p1)
-      games << build(:game, winner_one: p1, winner_two: p4, loser_one: p2, loser_two: p3)
+      games << build(:game, winner_one: @p1, winner_two: @p2, loser_one: @p3, loser_two: @p4)
+      games << build(:game, winner_one: @p1, winner_two: @p3, loser_one: @p2, loser_two: @p4)
+      games << build(:game, winner_one: @p3, winner_two: @p4, loser_one: @p2, loser_two: @p1)
+      games << build(:game, winner_one: @p1, winner_two: @p4, loser_one: @p2, loser_two: @p3)
 
       rankings = Game.calculate_rankings(games, 1200, 16, 400.0)
-      rankings[p1.id].should be_within(0.01).of(1215.26)
-      rankings[p2.id].should be_within(0.01).of(1183.26)
-      rankings[p3.id].should be_within(0.01).of(1200.73)
-      rankings[p4.id].should be_within(0.01).of(1200.73)
+      rankings[@p1.id].should be_within(0.01).of(1215.26)
+      rankings[@p2.id].should be_within(0.01).of(1183.26)
+      rankings[@p3.id].should be_within(0.01).of(1200.73)
+      rankings[@p4.id].should be_within(0.01).of(1200.73)
     end
 
-    it "rewards scores in a logarithmic curve"
+    it "rewards scores in a logarithmic curve" do
+
+    end
 
     it "rewards underdogs more than the favorites"
 
