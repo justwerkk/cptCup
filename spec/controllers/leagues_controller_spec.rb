8b9e0c0a3e1e9bff83bace3c1f53f8b02369da0c
Lijen Tan <lijentan@gmail.com>
2013-07-17 14:57:59 -0700

    completed index tests

--- spec/controllers/leagues_controller_spec.rb
+++ spec/controllers/leagues_controller_spec.rb
@@ -9,8 +9,15 @@ describe LeaguesController do
       assigns(:leagues).should eq(leagues.reverse)
     end
 
-    it "renders the :index view"
-    it "builds a new player"
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+    end
+
+    it "builds a new player" do
+      get :index
+      assigns(:player).should be_new_record
+    end
   end
 
 end
