a3355764745ab493529e8d258bd684ec7bec097d
Lijen Tan <lijentan@gmail.com>
2013-07-18 19:11:01 -0700

    order players by name, dont allow deletes for players with games

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,7 @@ class PlayersController < ApplicationController
   # GET /players
   # GET /players.json
   def index
-    @players = Player.all
+    @players = Player.order("name ASC")
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,9 @@
 class Player < ActiveRecord::Base
   attr_accessible :name
   validates_uniqueness_of :name
+  validates_presence_of :name
+
+  has_many :games, dependent: :restrict, :finder_sql =>
+    Proc.new {"SELECT * FROM games INNER JOIN players ON games.winner_one_id = #{id} OR games.winner_two_id = #{id} OR games.loser_one_id = #{id} OR games.loser_two_id = #{id}"}
 
 end
