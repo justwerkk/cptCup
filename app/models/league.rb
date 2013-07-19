b63c403ace28017b91ce9677b7a44d4e80f296da
Lijen Tan <lijentan@gmail.com>
2013-07-18 17:59:48 -0700

    restrict deleting of a league

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,6 +2,18 @@ class League < ActiveRecord::Base
   attr_accessible :end_date, :start_date, :name
   validates_presence_of :name, :start_date
 
-  has_many :games, order: "created_at ASC"
   default_scope order: 'start_date DESC'
+
+  has_many :games, order: "created_at ASC", dependent: :restrict
+
+  before_destroy :validate_not_completed
+
+  private
+
+  def validate_not_completed
+    unless end_date.nil?
+      self.errors.add(:base, "cant delete a league that's completed")
+      return false
+    end
+  end
 end
