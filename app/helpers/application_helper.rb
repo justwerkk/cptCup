b25d588034d8ad8f55424b7807ce502f50fa4882
Lijen Tan <lijentan@gmail.com>
2013-07-19 19:59:19 -0700

    fixing up game listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
   def leagues
     League.order("name ASC").collect {|l| [l.name, l.id]}
   end
+
+  def winner_string(game)
+    case game.is_team_one_victory
+    when nil
+      "Pending"
+    when true
+      "Team 1"
+    when false
+      "Team 2"
+    end
+  end
 end
